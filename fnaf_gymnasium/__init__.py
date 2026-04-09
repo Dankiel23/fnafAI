3406e10f1ee4e72e0f44c3c04cdf783fe9f8d676
Daniel Kassis <kassis.daniel@gmail.com>
2026-04-09 22:33:28 +1000

    Export wrappers from package init for easier imports

--- fnaf_gymnasium/__init__.py
+++ fnaf_gymnasium/__init__.py
@@ -1,9 +1,22 @@
 """FNAF 1 Gymnasium Environment for Reinforcement Learning."""
 
 from gymnasium.envs.registration import register
+from .wrappers import (
+    FnafRewardShaping,
+    FnafSparseReward,
+    FnafTimePenalty,
+    FnafActionMask,
+)
 
 register(
     id='FnafNight-v0',
     entry_point='fnaf_gymnasium.envs:FnafEnv',
     max_episode_steps=535,  # One step per second, 535 seconds per night
 )
+
+__all__ = [
+    'FnafRewardShaping',
+    'FnafSparseReward',
+    'FnafTimePenalty',
+    'FnafActionMask',
+]
